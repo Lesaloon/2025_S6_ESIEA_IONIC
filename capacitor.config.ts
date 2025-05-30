bd2c4fc9e54353c4ae6e64d1bd165521dd9dc876
Lesaloon <47109666+Lesaloon@users.noreply.github.com>
2025-05-30 11:49:05 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '2025_S6_ESIEA_IONIC',
+  webDir: 'www'
+};
+
+export default config;
