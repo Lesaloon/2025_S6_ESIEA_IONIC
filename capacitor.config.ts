b13946f8c340f5a4a366d0ceefc311e536a1815e
Lesaloon <47109666+Lesaloon@users.noreply.github.com>
2025-06-15 18:32:12 +0200

    feat: Add plugin configuration for SplashScreen in capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: '2025_S6_ESIEA_IONIC',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: '#ffffffff',
+      showSpinner: false,
+      androidSplashResourceName: 'splash'
+    }
+  }
 };
 
 export default config;
